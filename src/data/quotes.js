636954bea72d450a3d5036c0de64e142ffd7f955
Askhat Jussupov <acxatjussupov@gmail.com>
2025-12-06 18:20:12 +0500

    Initial version of the quotes app using OOP and classes - choosing random quote from the array of quotes

---
+++ src/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    id: 'd3f97c43',
+    text: 'The best way to get started is to quit talking and begin doing',
+    author: 'Walt Disney',
+  },
+  {
+    id: '147829ca',
+    text: "Don't let yesterday take up too much of today",
+    author: 'Will Rogers',
+  },
+  {
+    id: 'c0ceca0d',
+    text: "It's not whether you get knocked down, it's whether you get up",
+    author: 'Vince Lombardi',
+  },
+  {
+    id: '56e8c304',
+    text: 'If you are working on something exciting, it will keep you motivated',
+    author: 'Steve Jobs',
+  },
+  {
+    id: '4043afac',
+    text: "The harder you work for something, the greater you'll feel when you achieve it",
+    author: 'Unknown author',
+  },
+  {
+    id: '25f44e66',
+    text: 'Success is not final; failure is not fatal: It is the courage to continue that counts',
+    author: 'Winston Churchill',
+  },
+  {
+    id: '060298c7',
+    text: 'Your time is limited, so don’t waste it living someone else’s life',
+    author: 'Steve Jobs',
+  },
+  {
+    id: '4e92a0da',
+    text: 'In the middle of every difficulty lies opportunity',
+    author: 'Albert Einstein',
+  },
+  {
+    id: '8818f580',
+    text: 'Believe you can and you’re halfway there',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    id: '3b0de9fa',
+    text: 'If you want to achieve greatness, stop asking for permission',
+    author: 'Anonymous',
+  },
+];
+
+export default quotes;
