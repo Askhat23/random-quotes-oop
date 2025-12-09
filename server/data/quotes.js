bdbe82f4f4da64a7a56c1d4ba7e2a2e87a46d15c
Askhat Jussupov <acxatjussupov@gmail.com>
2025-12-09 15:56:26 +0500

    Creating own quotes API Node.js Express server with resource /quotes/random-single

---
+++ server/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    id: 'a1b23c9f',
+    text: 'What you do today can improve all your tomorrows',
+    author: 'Ralph Marston',
+  },
+  {
+    id: 'f94e12ad',
+    text: 'Act as if what you do makes a difference. It does',
+    author: 'William James',
+  },
+  {
+    id: '7c3d8e55',
+    text: 'It always seems impossible until it’s done',
+    author: 'Nelson Mandela',
+  },
+  {
+    id: 'e2a59d01',
+    text: 'Hardships often prepare ordinary people for an extraordinary destiny',
+    author: 'C.S. Lewis',
+  },
+  {
+    id: '31f7a4c8',
+    text: 'Don’t watch the clock; do what it does. Keep going',
+    author: 'Sam Levenson',
+  },
+  {
+    id: '9d80c1b3',
+    text: 'Success is walking from failure to failure with no loss of enthusiasm',
+    author: 'Winston Churchill',
+  },
+  {
+    id: '5bb7ef61',
+    text: 'Dream big and dare to fail',
+    author: 'Norman Vaughan',
+  },
+  {
+    id: 'c4f91e27',
+    text: 'Opportunities don’t happen. You create them',
+    author: 'Chris Grosser',
+  },
+  {
+    id: '82d0ac14',
+    text: 'Everything you can imagine is real',
+    author: 'Pablo Picasso',
+  },
+  {
+    id: '6ea43bf9',
+    text: 'Quality is not an act, it is a habit',
+    author: 'Aristotle',
+  },
+];
+
+module.exports = quotes;
