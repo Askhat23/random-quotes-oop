2e6c6e2d7adf72ae976f7e59a61541dde5ab8053
Askhat Jussupov <acxatjussupov@gmail.com>
2025-12-10 22:33:35 +0500

    Add README.md and config in the client app

---
+++ client/src/config.js
@@ -0,0 +1,8 @@
+const PUBLIC_API_URL = 'https://quoteslate.vercel.app';
+const OWN_API_URL = 'http://localhost:3000';
+
+const config = {
+  PUBLIC_API_URL,
+  OWN_API_URL,
+};
+export default config;
